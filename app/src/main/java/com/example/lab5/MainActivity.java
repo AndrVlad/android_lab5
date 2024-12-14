df0a5329c81f9e86342fc059bf6242bffdce0511
Andrianooo <vladosandrianov@yandex.ru>
2024-12-14 13:13:49 +0300

    Сделал UI

--- app/src/main/java/com/example/lab5/MainActivity.java
+++ app/src/main/java/com/example/lab5/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edit = (EditText) findViewById(R.id.editTextText);
+    String journal_val = edit.getText().toString();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickBtn1(View view) {
+
     }
 }
